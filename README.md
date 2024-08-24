df9459ed69579db7138b41f81e1ebcbf75efd0cc
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2024-08-24 22:58:49 +0200

    Setup code structure

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# ViaVai
+# Testing - Core functionality of ViaVai
+
+The goal is to compile the react and js code in tree separate parts.
+1. The `.dist/libs`s folder contains the `min.js` files needed for each library.
+   To ensures that the libraries are only loaded once.
+
+2. The `.dist/bundle` folder contains the `bundle.js` file that is the entry file. It contains the logic to connect with the python backend and load the components dynamically.
+
+3. The `.dist/components` folder contains the `components` files that are the components that are loaded dynamically, those are imported in the frontend only when needed.
+
+
+<br />
+
+## Build
+
+To compile the libraries run
+```bash
+npm run libs
+```
+
+To compile the main run
+```bash
+npm run main
+```
+
+To compile the components run
+```bash
+npm run components
+```
+
+To compile all run
+```bash
+npm run build
+```
+
+
+<br />
+
+## Run 
+
+To run the pytohn flask server run
+```bash
+python server.py
+```
--- setup.py
+++ setup.py
@@ -2,21 +2,19 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='viavai',  # Name of your package on PyPI
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    description='A brief description of my package',
+    name="viavai",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A sample Python library called Viavai",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author='Your Name',
-    author_email='your.email@example.com',
-    url='https://github.com/yourusername/my_project',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},  # Point to the src directory
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/viavai",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.11',
 )
