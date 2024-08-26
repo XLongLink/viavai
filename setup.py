d798cb228d1fcb46b271177a92559939e069607a
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2024-08-26 21:31:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="viavai",
-    version="0.1.0",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     packages=find_packages(),
     description="A sample Python library called Viavai",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    author="Your Name",
+    author="Sau1707",
     author_email="your.email@example.com",
-    url="https://github.com/yourusername/viavai",
+    url="https://github.com/XLongLink/viavai",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
