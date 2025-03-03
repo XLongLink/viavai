e285288b4a9b5e32e7cc3aa1173d6bf72d4bde22
Leonardo Saurwein <leonardo@saurwein.ch>
2025-03-03 20:31:06 +0100

    Sync

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import Page from "@/app/dashboard/page"
+import { ThemeProvider } from "@/components/theme-provider"
+
+export default function App() {
 
     return (
-        <>
-        </>
+        <ThemeProvider>
+            <Page />
+        </ThemeProvider>
     )
 }
-
-export default App
--- view/vite.config.ts
+++ view/vite.config.ts
@@ -1,16 +1,15 @@
 import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
 	build: {
-		outDir: '../viavai/static', // Change this to your preferred folder name
+		outDir: path.resolve(__dirname, '../viavai/static'),
 	},
 });
