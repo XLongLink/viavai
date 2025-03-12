c2f98634df7783b636ab5a2161574a717b677ea4
Leonardo Saurwein <leonardo@saurwein.ch>
2025-03-12 21:00:12 +0100

    Edit examples

--- examples/nav.py
+++ examples/nav.py
@@ -9,16 +9,19 @@ class ViavaiApp(App):
     subtitle = "Admin Portal"
     
     def __init__(self):
+        # This is an example of a static navbar. 
+        # Another example is to fetch data from a database and then dynamically create the navbar
+        
         # Create navbar section for active projects
         section = self.add_section("Projects", "plus")
-        section.add_item("000 Cureggia", icon="house", path="/projects/000-cureggia")
-        section.add_item("001 Paradiso", icon="clipboard-list", path="/projects/001-paradiso")
-        section.add_item("002 Brè", icon="folder-open-dot", path="/projects/002-bre")
+        section.add_item("000 NewYork", icon="house", path="/projects/000")
+        section.add_item("001 London", icon="clipboard-list", path="/projects/001")
+        section.add_item("002 Zurich", icon="folder-open-dot", path="/projects/002")
 
         # Create navbar section for archived projects
         section = self.add_section("Archive", "collapse")
-        section.add_item("201 Valcolla", icon="home", path="/archive/201-valcolla")
-        section.add_item("202 Viganello", icon="home", path="/archive/202-viganello")
+        section.add_item("201 Paris", icon="home", path="/archive/201")
+        section.add_item("202 Berlin", icon="home", path="/archive/202")
 
 
 if __name__ == '__main__':
