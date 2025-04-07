60d4c5f7a7062712ce27510cb2a8b959a5caa7db
Leonardo Saurwein <leonardo@saurwein.ch>
2025-04-07 22:17:03 +0200

    Deploy website - based on 198735bac850ced409fee2a2cbcc3f555cf25a65

---
+++ assets/js/a7456010.f5875dc2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus_tailwind_shadcn_template=self.webpackChunkdocusaurus_tailwind_shadcn_template||[]).push([[235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
